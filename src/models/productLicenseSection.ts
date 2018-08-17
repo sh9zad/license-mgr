5f5ca6a8e252b87c7a6a4aa068278ff63cfedbde
Shervin <shassanzadeh@videoamp.com>
2018-08-16 17:34:07 -0700

    Model update

--- src/models/productLicenseSection.ts
+++ src/models/productLicenseSection.ts
@@ -2,18 +2,14 @@ import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-export const productLiceseSectionSchema = new Schema({
-    account_id: {
+export const productLicenseSectionSchema = new Schema({
+    product_id: {
         type: Number,
         required: 'Enter a company name'
     },
-    product_id: {
+    license_section_id: {
         type: Number,
         required: 'Enter a client code'
-    },
-    license: {
-        type: String,
-        required: ''
     },
     created_date: {
         type: Date,
