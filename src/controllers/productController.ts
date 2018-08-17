c5b328485872dfb7942477a684054487936db757
Shervin <shassanzadeh@videoamp.com>
2018-08-17 08:40:07 -0700

    Controller and routes updated.

---
+++ src/controllers/productController.ts
@@ -0,0 +1,56 @@
+import * as mongoose from 'mongoose';
+import { ProductSchema } from '../models/productModel';
+import { Request, Response } from 'express';
+
+const Contact = mongoose.model('Product', ProductSchema);
+
+export class ProductController{
+
+    public addNewProduct (req: Request, res: Response) {                
+        let newContact = new Contact(req.body);
+    
+        newContact.save((err, contact) => {
+            if(err){
+                res.send(err);
+            }    
+            res.json(contact);
+        });
+    }
+
+    public getProducts (req: Request, res: Response) {           
+        Contact.find({}, (err, contact) => {
+            if(err){
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+
+    public getProductWithID (req: Request, res: Response) {           
+        Contact.findById(req.params.contactId, (err, contact) => {
+            if(err){
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+
+    public updateProduct (req: Request, res: Response) {           
+        Contact.findOneAndUpdate({ _id: req.params.contactId }, req.body, { new: true }, (err, contact) => {
+            if(err){
+                res.send(err);
+            }
+            res.json(contact);
+        });
+    }
+
+    public deleteProduct (req: Request, res: Response) {           
+        Contact.remove({ _id: req.params.contactId }, (err, contact) => {
+            if(err){
+                res.send(err);
+            }
+            res.json({ message: 'Successfully deleted contact!'});
+        });
+    }
+    
+}
