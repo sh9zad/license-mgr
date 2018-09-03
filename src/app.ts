6b59b249db3a569f2643b5968a5ca1fe012b86f6
Shervin <shervin.hassanzadeh@fayebsg.com>
2018-09-02 18:38:03 -0700

    CSRF stuff fixed.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import * as bodyParser from "body-parser";
 import { ProductRoutes } from "./routes/productsRoutes";
 import { AccountRoutes } from "./routes/accountRoutes";
 import * as mongoose from "mongoose";
+import * as csrf from "csurf";
+import * as cookieParser from 'cookie-parser';
+
+
 
 class App {
 
@@ -10,6 +14,7 @@ class App {
     public productRoutes: ProductRoutes = new ProductRoutes();
     public accountRoutes: AccountRoutes = new AccountRoutes();
     public mongoUrl: string = 'mongodb://localhost:27017/license_mgr';
+    // public csrf = csrf();
 
     constructor() {
         this.app = express();
@@ -22,6 +27,17 @@ class App {
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(cookieParser());
+        //this.app.use(csrf({ cookie: false, ignoreMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'] }))
+        this.app.use(function (req, res, next) {
+            console.log('we have been here');
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+            next();
+        });
+
         // serving static files 
         this.app.use(express.static('public'));
     }
