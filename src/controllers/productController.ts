0b94088a4f5cac09b6077c0a24183f61a9743ed9
Shervin <shassanzadeh@videoamp.com>
2018-09-14 16:26:13 -0700

    Updated code.

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,56 +1,59 @@
-import * as mongoose from 'mongoose';
-import { ProductSchema } from '../models/productModel';
-import { Request, Response } from 'express';
+import * as mongoose from "mongoose";
+import { ProductSchema } from "../models/productModel";
+import { Request, Response } from "express";
 
-const Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model("Product", ProductSchema);
 
-export class ProductController{
+export class ProductController {
+  public addNew(req: Request, res: Response) {
+    let newItem = new Product(req.body);
 
-    public addNew (req: Request, res: Response) {                
-        let newItem = new Product(req.body);
-    
-        newItem.save((err, item) => {
-            if(err){
-                res.send(err);
-            }    
-            res.json(item);
-        });
-    }
+    newItem.save((err, item) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(item);
+    });
+  }
 
-    public get (req: Request, res: Response) {           
-        Product.find({}, (err, item) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(item);
-        });
-    }
+  public get(req: Request, res: Response) {
+    Product.find({}, (err, item) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(item);
+    });
+  }
 
-    public getWithID (req: Request, res: Response) {           
-        Product.findById(req.params.contactId, (err, item) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(item);
-        });
-    }
+  public getWithID(req: Request, res: Response) {
+    Product.findById(req.params.id, (err, item) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(item);
+    });
+  }
 
-    public update (req: Request, res: Response) {           
-        Product.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, item) => {
-            if(err){
-                res.send(err);
-            }
-            res.json(item);
-        });
-    }
+  public update(req: Request, res: Response) {
+    Product.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true },
+      (err, item) => {
+        if (err) {
+          res.send(err);
+        }
+        res.json(item);
+      }
+    );
+  }
 
-    public delete (req: Request, res: Response) {           
-        Product.remove({ _id: req.params.id }, (err) => {
-            if(err){
-                res.send(err);
-            }
-            res.json({ message: 'Successfully deleted the item!'});
-        });
-    }
-    
+  public delete(req: Request, res: Response) {
+    Product.remove({ _id: req.params.id }, err => {
+      if (err) {
+        res.send(err);
+      }
+      res.json({ message: "Successfully deleted the item!" });
+    });
+  }
 }
--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -2,40 +2,30 @@ import { Request, Response, NextFunction } from "express";
 import { ProductController } from "../controllers/productController";
 
 export class ProductRoutes {
+  public controller: ProductController = new ProductController();
 
-    public controller: ProductController = new ProductController()
+  public routes(app): void {
+    app.route("/").get((req: Request, res: Response) => {
+      res.status(200).send({
+        message: "GET request successfulll!!!!"
+      });
+    });
 
-    public routes(app): void {   
-        
-        app.route('/')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                message: 'GET request successfulll!!!!'
-            })
-        })
-        
-        // Contact 
-        app.route('/product')
-        .get((req: Request, res: Response, next: NextFunction) => {
-            // middleware
-            // console.log(`Request from: ${req.originalUrl}`);
-            // console.log(`Request type: ${req.method}`);            
-            // if(req.query.key !== '78942ef2c1c98bf10fca09c808d718fa3734703e'){
-            //     res.status(401).send('You shall not pass!');
-            // } else {
-            //     next();
-            // }    
-            next();
-        }, this.controller.get)        
+    // Contact
+    app
+      .route("/product")
+      .get((req: Request, res: Response, next: NextFunction) => {
+        next();
+      }, this.controller.get)
 
-        // POST endpoint
-        .post(this.controller.addNew);
+      // POST endpoint
+      .post(this.controller.addNew);
 
-        // Contact detail
-        app.route('/product/:id')
-        .get(this.controller.getWithID)
-        .put(this.controller.update)
-        .delete(this.controller.delete)
-
-    }
+    // Contact detail
+    app
+      .route("/product/:id")
+      .get(this.controller.getWithID)
+      .put(this.controller.update)
+      .delete(this.controller.delete);
+  }
 }
